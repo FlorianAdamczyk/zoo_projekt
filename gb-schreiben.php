c9b71af6d9e76af5588033253040ceba9e5bf7f0
Flo <90013108+FlorianAdamczyk@users.noreply.github.com>
2024-05-03 23:34:52 +0200

    aufgaben 3 von iryna übernommen

---
+++ gb-schreiben.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang=de>
+
+<head>
+	<title> Zoo Zarathustra </title>
+	<meta charset=utf-8>
+</head>
+
+<body>
+<h1>Gästebuch</h1>
+Vielen Dank für Ihren Gästebuch-Eintrag!
+
+	<br><br><br><br><br><br><br><br>
+	<br><br><br><br><br><br><br><br>
+	<br><br><br><br><br><br><br><br>
+	<br><br><br><br><br><br><br><br>
+	<br><br><br><br><br><br><br><br>
+
+	<h4> Impressum </h4>
+	<a id="impressum">
+		<i> Justin Iglhaut, Iryna Korchynska, Florian Adamczyk </i> <br> &copy; <b>2024</b>
+	</a>
+
+</body>
+
+</html>
