5dc40a2afd0ae3abded3f47bed0de63b5414d4fa
Flo <90013108+FlorianAdamczyk@users.noreply.github.com>
2024-06-11 01:42:20 +0200

    Fibonacci finalisiert.

--- Fibonacci.js
+++ Fibonacci.js
@@ -1,11 +1,10 @@
-function Fibonacci(n) {
-    if (n <= 1) {
-        return n;
-    }
+function Fibonacci(n) {  //Gibt die n-te Fibonacci Zahl aus: mit Rekursion ist es am wenigsten Arbeit.
+    if (n <= 1) return n;
     return Fibonacci(n - 1) + Fibonacci(n - 2);
 }
 
-function test(n) {
-    return n;
+function FibonacciRange(n) { // Funktion zur Ausgabe der Fibonacci-Zahlen
+    for (let i = 1; i <= n; i++) {
+        document.write(Fibonacci(i)+ "<br>");
+    }
 }
-
